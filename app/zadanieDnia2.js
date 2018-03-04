2056f98879be6f192e219ed7b8d940c5849872f6
vinilll <dzisvolodymyr@gmail.com>
2018-03-04 14:20:09 +0100

    Day 4 done!

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,13 @@
 //Twój kod
+
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const user_agent = req.headers['user-agent'];
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(user_agent);
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
